a4862ad76b1294f74b6bc4232b84d65a74dec481
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2022-12-20 14:46:54 +0100

    6 task fix

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,7 @@ function onChangeColor(event) {
     validationInput.classList.remove('valid')
     validationInput.classList.remove('invalid')
     
-    if (event.currentTarget.value.length > 6) {
+    if (event.currentTarget.value.length > 6 || event.currentTarget.value.length < 6) {
         validationInput.classList.add('invalid')
     } if (event.currentTarget.value.length === 6) {
         validationInput.classList.add('valid')
